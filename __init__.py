cd3898097076e69bc9c83d27cc19ec9c145e715f
黄集攀 <564298339@qq.com>
2020-10-12 16:37:21 +0800

    update __init__.py.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
-import os,sys,time,json,re
+import os, sys, time, json, re
 # import win32.win32clipboard as clipboard 
 # import the main window object (mw) from aqt
-from aqt import mw,gui_hooks
+from aqt import mw, gui_hooks
+from html.parser import HTMLParser
 from aqt.browser import Browser
-from aqt.utils import showInfo,tooltip
+from aqt.utils import showInfo, tooltip
 from aqt.qt import *
+
 from anki import hooks
 import html
 import time
@@ -12,217 +14,272 @@ from dataclasses import dataclass
 from enum import Enum
 from operator import itemgetter
 from typing import Callable, List, Optional, Sequence, Tuple, Union
-helpSite= "https://gitee.com/huangjipan/hjp-bilink"
-inputFileName="test.txt"
-configFileName="config.json"
-helpFileName="README.md"
-relyLinkDir="1423933177"
-relyLinkConfigFileName="config.json"
+
+helpSite = "https://gitee.com/huangjipan/hjp-bilink"
+inputFileName = "input.json"
+configFileName = "config.json"
+helpFileName = "README.md"
+relyLinkDir = "1423933177"
+relyLinkConfigFileName = "config.json"
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PREV_FOLDER = os.path.dirname(THIS_FOLDER)
-RELY_FOLDER = os.path.join(PREV_FOLDER,relyLinkDir)
-errorType=[
-    "card_id格式不正确","colSep分栏不正确","记录为空!"
+RELY_FOLDER = os.path.join(PREV_FOLDER, relyLinkDir)
+errorType = [
+    "card_id格式不正确", "colSep分栏不正确", "记录为空!"
 ]
 
 
 class Link(object):
-    def __init__(self,path,cfgpath,relycfgpath,prefix_cid="prefix_cid",defaultMode=999):
-        self.path=path
-        self.cfgpath=cfgpath
-        self.confg=json.load(open(cfgpath,"r",encoding="utf-8"))
-        self.relycfg=json.load(open(relycfgpath,"r",encoding="utf-8"))
-
-        self.rowSep=self.confg["rowSeparator"]
-        self.colSep=self.confg["colSeparator"]
-        if defaultMode==999:
-            self.mode=self.confg["linkMode"]  
+    def __init__(self, path, cfgpath, relycfgpath, prefix_cid="prefix_cid", defaultMode=999):
+        self.path = path
+        self.cfgpath = cfgpath
+        self.confg = json.load(open(cfgpath, "r", encoding="utf-8"))
+        self.relycfg = json.load(open(relycfgpath, "r", encoding="utf-8"))
+        self.rowSep = self.confg["rowSeparator"]
+        self.colSep = self.confg["colSeparator"]
+        if defaultMode == 999:
+            self.mode = self.confg["linkMode"]
         else:
-            self.mode=defaultMode
-        
-        self.prefix=self.confg["cidPrefix"]
-        self.fieldPosi=self.confg["appendNoteFieldPosition"]
-        self.mapFuncPath=[
-            self.completemap,
-            self.groupBygroup
+            self.mode = defaultMode
+
+        self.prefix = self.confg["cidPrefix"]
+        self.fieldPosi = self.confg["appendNoteFieldPosition"]
+        self.mapFuncPath = [
+            self.completemap,  # mode0
+            self.groupBygroup,  # mode1
+            self.unlinkNode,  # mode2
+            self.unlinkPath,#mode3
         ]
         if self.prefix != self.relycfg[prefix_cid]:
-            self.relycfg[prefix_cid]=self.prefix
-            json.dump(self.relycfg,open(relycfgpath,"w",encoding="utf-8"))
-
-        with open(path,"r",encoding="utf-8") as fdata:
-            fdata=fdata.read()
-            fdata=re.sub(self.rowSep+self.rowSep+"{2,}",self.rowSep+self.rowSep,fdata)
-            fdata=re.sub("^"+self.rowSep+"{1,}",'',fdata)
-            fdata=re.sub(self.rowSep+"{1,}$",'',fdata)
-            showInfo(fdata+",len="+str(len(fdata.split(self.rowSep))))
-            self.fdata=fdata
-            self.cidli=fdata.split(self.rowSep)
+            self.relycfg[prefix_cid] = self.prefix
+            json.dump(self.relycfg, open(relycfgpath, "w", encoding="utf-8"))
+        self.fdata = json.load(open(os.path.join(THIS_FOLDER, inputFileName), "r", encoding="utf-8"))
 
     def start(self):
-        errorcode=self.safeCheck()
-        if errorcode!=-1:
-            showInfo(f"errorcode:{str(errorcode)}\n{errorType[errorcode]}")
+        # errorcode=self.safeCheck()
+        # if errorcode!=-1:
+        #     showInfo(f"errorcode:{str(errorcode)}\n{errorType[errorcode]}")
+        #     return
+        if len(self.fdata["IdDescPairs"]) == 0 and len(self.fdata["IdDescGroups"]) == 0:
+            showInfo("no data！")
             return
-        showInfo("mode:"+str(int(self.mode))+",start")
+        showInfo("mode:" + str(int(self.mode)) + ",start")
         self.mapFuncPath[self.mode]()
         showInfo("finished!")
 
-    def appendIDtoNote(self,note,content):
+    # 下面的是工具
+    def getCardNoteFromId(self, li: int) -> object:
         '''
-        note必须是一个cardnote,posi是一个cardnote的位置
+         @li
+        li是一个number
         '''
-        Id_DescribePair=content.split(self.colSep)
-        Id=Id_DescribePair[0]
-        Desc= Id_DescribePair[1] if len(Id_DescribePair)>1 else ''
-        note.fields[self.fieldPosi]+=f"<div>{self.prefix}{Id} {Desc}</div>\n"
+        return mw.col.getCard(li).note()
+
+    def appendIDtoNote(self, note, IdDescPair):
+        '''
+        note必须是一个cardnote,posi是一个cardnote的位置,Id_DescribePair是卡号、描述的键值对
+        '''
+        Id = IdDescPair["card_id"]
+        descstr = self.getCardNoteFromId(IdDescPair["card_id"]).fields[self.confg["readDescFieldPosition"]]
+        seRegx = self.confg["DEFAULT"]["regexForDescContent"] if self.confg["regexForDescContent"] == 0 else self.confg[
+            "regexForDescContent"]
+        Desc = IdDescPair["desc"] if len(IdDescPair["desc"]) > 1 else re.search(seRegx, descstr)[0]
+        note.fields[self.fieldPosi] += f"<div card_id='{Id}'>{Desc} {self.prefix}{Id}</div>\n"
         note.flush()
 
-    def AmapB(self,cidli):
+    def getCardIDfromNote(self, id : int) -> list:
+        note = self.getCardNoteFromId(id)
+        content = note.fields[self.fieldPosi]
+        return re.findall(f"(?<={self.prefix})" + r"\d{13}", content)
+
+    # 下面的是链接算法
+    def AmapB(self, groupA, groupB):
         '''
         A位置的所有ID插入到B中,并作一个B的反向链接回A.
-        TXT格式必须是AmapB起头,然后A位置输完要给一个换行符
+        json格式必须是AmapB起头,然后A位置输完要给一个换行符
         这个以后要大改.改好了
         '''
-        fieldPosi=self.fieldPosi
-        posi=0 #确定位置
-        for cid in cidli:
-            if cid == "":
-                break
-            posi += 1
-        for BcardId in cidli[posi+1:]:
-            Bnote=self.getCardNoteFromStr(BcardId)
-            for AcardId in cidli[0:posi]:
-                if re.search(AcardId,Bnote.fields[fieldPosi]) is None:
-                #if not (AcardId in Bnote.fields[fieldPosi]):
-                    self.appendIDtoNote(Bnote,AcardId)
-                Anote=self.getCardNoteFromStr(AcardId)
-                if re.search(BcardId,Anote.fields[fieldPosi]) is None:
-                #if not (BcardId in Anote.fields[fieldPosi]):
-                    self.appendIDtoNote(Anote,BcardId)              
+        fieldPosi = self.fieldPosi
+        for idpA in groupA:
+            Anote = self.getCardNoteFromId(idpA["card_id"])
+            for idpB in groupB:
+                Bnote = self.getCardNoteFromId(idpB["card_id"])
+                if re.search(str(idpB["card_id"]), Anote.fields[fieldPosi]) is None:
+                    self.appendIDtoNote(Anote, idpB)
+                if re.search(str(idpA["card_id"]), Bnote.fields[fieldPosi]) is None:
+                    self.appendIDtoNote(Bnote, idpA)
+
     def completemap(self):
         '''
         完全图,没什么说的
         '''
-        cidli=self.cidli
-        fieldPosi=self.fieldPosi
-        for cid in cidli:
-            if cid=='':
+        cidli = self.fdata["IdDescPairs"]
+        fieldPosi = self.fieldPosi
+        for cidP in cidli:
+            if cidP["card_id"] == 0:
                 continue
-            note=self.getCardNoteFromStr(cid)
+            # 笔记是一个对象
+            note = self.getCardNoteFromId(cidP["card_id"])
             for linkcid in cidli:
-                #怎样才能访问到fields,需要用数字下标,访问到的是HTML模式
-                if cid != linkcid and (re.search(linkcid,note.fields[fieldPosi]) is None): #(linkcid  in note.fields[fieldPosi]):
-                    self.appendIDtoNote(note,linkcid)       
-    def safeCheck(self):
-        #cardIDCheck
-        countRowSep=0
-        emptyposi=[]
-        # if len(self.cidli)==1 and self.cidli[0]=='':
-        #     return 2 #记录为空!
-        for i in range(0,len(self.cidli)):
-            cid=self.cidli[i]
-            if cid=="":
-                countRowSep+=1
-                emptyposi.append(i)
-                continue
-            if not cid[0:13].isdigit():
-                return 0#id格式不正确
-            if len(cid.split(self.colSep))>2:
-                return 1#记录分段格式不正确
-        if countRowSep==len(self.cidli):
-            return 2 #记录为空!
-        return -1
+                # 怎样才能访问到fields,需要用数字下标,访问到的是HTML模式
+                IdA = cidP["card_id"]
+                IdB = linkcid["card_id"]
+                if IdA != IdB and (re.search(str(IdB), note.fields[fieldPosi]) is None):
+                    self.appendIDtoNote(note, linkcid)
 
     def groupBygroup(self):
         '''
         将其首尾分段相连A-B-C
         '''
-        cidli=self.cidli
-        groupidx=[]
-        for idx in range(1, len(cidli)):
-            if cidli[idx] == '':
-                groupidx.append(idx)
-            idx+=1
-        if len(groupidx)<=1:
-            self.AmapB(cidli)
-        else:#说明groupidx 至少2个
-            groupidx=[-1]+groupidx+[len(cidli)]# 至少4个
-            liA=[]
-            liB=[]
-            for i in range(0,len(groupidx)-2):#0,1,2起步
-                liA=cidli[groupidx[i]+1:groupidx[i+1]]
-                liB=cidli[groupidx[i+1]+1:groupidx[i+2]]
-                self.AmapB(liA+[""]+liB)
-    def getCardNoteFromStr(self,li):
-        '''
-        li必须是开头含cardID的字符串
-        '''
-        return mw.col.getCard(int(li.split(self.colSep)[0])).note()        
+        cidli = self.fdata["IdDescGroups"]
+        placeholder = {"card_id": 0, "desc": ""}
+        if len(cidli) < 2:
+            showInfo("link failed, not enough groups!")
+            return
+        else:  # 说明groupSeperIdx 至少2个
+            for i in range(0, len(cidli) - 1):  # 0,1,2起步
+                liA = cidli[i]
+                liB = cidli[i + 1]
+                self.AmapB(liA, liB)
+
+    def unlinkNode(self):
+        idpli = self.fdata["IdDescPairs"]
+        for idp in idpli:
+            id = str(idp["card_id"])
+            linkli = self.getCardIDfromNote(int(id))
+            # showInfo(str(idp["card_id"])+" listlen:"+str(len(linkli)))
+            for link in linkli:
+                # showInfo("this is "+link)
+                note = self.getCardNoteFromId(int(link))  # 链到的卡片上找自己
+                # test=re.search(f'''<div card_id=["']{id}["']>[\\s\\S]+?{id}</div>''',note.fields[self.fieldPosi])[0]
+                # showInfo(test)
+                content = re.sub(f'''<div card_id=["']{id}["']>[\\s\\S]+?{id}</div>''', "", note.fields[self.fieldPosi])
+                note.fields[self.fieldPosi] = content
+                note.flush()
+                note = self.getCardNoteFromId(idp["card_id"])
+                content = re.sub(f'''<div card_id=["']{link}["']>[\\s\\S]+?{link}</div>''', "",
+                                 note.fields[self.fieldPosi])
+                # showInfo(content)
+                note.fields[self.fieldPosi] = content
+                note.flush()
+
+    def unlinkPath(self):
+        idpli = self.fdata["IdDescPairs"]
+        for i in range(0, len(idpli) - 1):
+            idA = idpli[i]["card_id"]
+            idB = idpli[i + 1]["card_id"]
+            noteA = self.getCardNoteFromId(idA)
+            noteB = self.getCardNoteFromId(idB)
+            content = re.sub(f'''<div card_id=["']{str(idA)}["']>[\\s\\S]+?{str(idA)}</div>''', "",
+                             noteB.fields[self.fieldPosi])
+            noteB.fields[self.fieldPosi] = content
+            noteB.flush()
+            content = re.sub(f'''<div card_id=["']{str(idB)}["']>[\\s\\S]+?{str(idB)}</div>''', "",
+                             noteA.fields[self.fieldPosi])
+            noteA.fields[self.fieldPosi] = content
+            noteA.flush()
 
 
 def setupFunction(mode=999):
-    txt=os.path.join(THIS_FOLDER,inputFileName)
-    cfg=os.path.join(THIS_FOLDER,configFileName)
-    relycfg=os.path.join(RELY_FOLDER,relyLinkConfigFileName)
-    Linker=Link(txt,cfg,relycfg,defaultMode=mode)
+    input = os.path.join(THIS_FOLDER, inputFileName)
+    cfg = os.path.join(THIS_FOLDER, configFileName)
+    relycfg = os.path.join(RELY_FOLDER, relyLinkConfigFileName)
+    Linker = Link(input, cfg, relycfg, defaultMode=mode)
     Linker.start()
 
+
 def destroyFuntion():
-    fdata=open(os.path.join(THIS_FOLDER,inputFileName),"w",encoding="utf-8")
-    fdata.write("")
+    fdata = open(os.path.join(THIS_FOLDER, inputFileName), "w", encoding="utf-8")
+    fdata.write('{"IdDescPairs":[],\n"IdDescGroups":[]}')
     fdata.close()
     showInfo("cleared")
 
-def multicopyFunction(self):
+
+# mw.col.getCard(li).note()
+def multicopyFunction(self, groupCopy=False):
+    cfgpath = os.path.join(THIS_FOLDER, configFileName)
+    confg = json.load(open(cfgpath, "r", encoding="utf-8"))
+    s = json.load(open(os.path.join(THIS_FOLDER, inputFileName), "r", encoding="utf-8"))
+    group = []
     browser = self
-    s=""
-    if len(browser.selectedCards())==0:
+    if len(browser.selectedCards()) == 0:
         showInfo("nothing to copy!")
-        return 
-    for card_id in browser.selectedCards():#取出来的是id
-        s+="\n"+str(card_id)
-    fdata=open(os.path.join(THIS_FOLDER,inputFileName),"a",encoding="utf-8")
-    fdata.write(s)
-    fdata.close()
-    showInfo(s[1:]+"\nhas been append to the txt file")
+        return
+    for card_id in browser.selectedCards():
+        note = mw.col.getCard(card_id).note()  # 读取卡片
+        content = note.fields[confg["appendNoteFieldPosition"]]  # 读取字段
+        seRegx = confg["DEFAULT"]["regexForDescContent"] if confg["regexForDescContent"] == 0 else confg[
+            "regexForDescContent"]  # 读取正则规则
+        Desc = re.search(seRegx, content)[0]  # 综上读取描述文字
+        pair = {"card_id": card_id, "desc": Desc}
+        if groupCopy:
+            group.append(pair)
+        else:
+            s["IdDescPairs"].append(pair)
+    if len(group) > 0:
+        s["IdDescGroups"].append(group)
+    json.dump(s, open(os.path.join(THIS_FOLDER, inputFileName), "w", encoding="utf-8"), indent=4, ensure_ascii=False)
+    showInfo(str(len(browser.selectedCards())) + " card has been appended to the json file")
+    # if groupCopy:
+    #     group=[]
+    #     for card_id in browser.selectedCards():
+    #         group.append({"card_id":card_id,"desc":""})
+    #     s["IdDescGroups"].append(group)
+    #     json.dump(s, open(os.path.join(THIS_FOLDER, inputFileName), "w", encoding="utf-8"), indent=4)
+    #     showInfo("groupCopy")
+    #     return
+    # for card_id in browser.selectedCards():#取出来的是id
+    #     pair={"card_id":card_id,"desc":""}
+    #     s["IdDescPairs"].append(pair)
+
 
 def displayFunction():
-    Url=QUrl.fromLocalFile(""+os.path.join(THIS_FOLDER,inputFileName))
-    QDesktopServices.openUrl(Url)
-def configFunction():
-    Url=QUrl.fromLocalFile(""+os.path.join(THIS_FOLDER,configFileName))
-    QDesktopServices.openUrl(Url)
-def helpFunction():
-    #config=json.load(open(os.path.join(THIS_FOLDER,configFileName),"r",encoding="utf-8"))
-    #Url=QUrl.fromLocalFile(""+os.path.join(THIS_FOLDER,helpFileName))
-    Url=QUrl(helpSite)
+    Url = QUrl.fromLocalFile("" + os.path.join(THIS_FOLDER, inputFileName))
     QDesktopServices.openUrl(Url)
 
-menuActionCollect={}
-menuToolsItems={
-    'linkDefault':lambda m=999:setupFunction(mode=999),
-    'linkAll':lambda m=0:setupFunction(mode=0),
-    'linkGroupToGroup':lambda m=1:setupFunction(mode=1),
-    'clear':destroyFuntion,
-    'show':displayFunction,
-    'config':configFunction,
-    'help':helpFunction,
+
+def configFunction():
+    Url = QUrl.fromLocalFile("" + os.path.join(THIS_FOLDER, configFileName))
+    QDesktopServices.openUrl(Url)
+
+
+def helpFunction():
+    # config=json.load(open(os.path.join(THIS_FOLDER,configFileName),"r",encoding="utf-8"))
+    # Url=QUrl.fromLocalFile(""+os.path.join(THIS_FOLDER,helpFileName))
+    Url = QUrl(helpSite)
+    QDesktopServices.openUrl(Url)
+
+
+menuActionCollect = {}
+menuToolsItems = {
+    'linkDefault': lambda m=999: setupFunction(mode=999),
+    'linkAll': lambda m=0: setupFunction(mode=0),
+    'linkGroupToGroup': lambda m=1: setupFunction(mode=1),
+    'unlinkNode': lambda m=2: setupFunction(mode=2),
+    'unlinkPath': lambda m=3: setupFunction(mode=3),
+    'clear': destroyFuntion,
+    'show': displayFunction,
+    'config': configFunction,
+    'help': helpFunction,
 }
 
 '''
 
 '''
+
+
 def linkActToMainMenu():
     '''
     将函数与钩子配对加到菜单里
     '''
-    for name,action in  menuToolsItems.items():
-        menuActionCollect[name]=QAction(name,mw)
+    for name, action in menuToolsItems.items():
+        menuActionCollect[name] = QAction(name, mw)
         menuActionCollect[name].triggered.connect(action)
         mw.form.menuTools.addAction(menuActionCollect[name])
+
+
 def setUpMenuShortcut(self):
-    #将参数命名为browser
+    # 将参数命名为browser
     browser = self
     '''
     #如果browser存在,直接让m读取menulinking属性,如果不存在,我们就自己创建一个
@@ -236,15 +293,21 @@ def setUpMenuShortcut(self):
         self.hjp_Link = QMenu("hjp_link")
         self.menuBar().insertMenu(self.mw.form.menuTools.menuAction(), self.hjp_Link)
         m = self.hjp_Link
-    for name,action in  menuToolsItems.items():
-        menuActionCollect[name]=QAction(name,mw)
+    for name, action in menuToolsItems.items():
+        menuActionCollect[name] = QAction(name, mw)
         menuActionCollect[name].triggered.connect(action)
         m.addAction(menuActionCollect[name])
-def AddToTableContextMenu(browser,menu):
-    actionCopyCidAll = QAction("hjpCopyCidAlltoTxt",browser)
-    actionCopyCidAll.triggered.connect(lambda _, b=browser:multicopyFunction(b))
+
+
+def AddToTableContextMenu(browser, menu):
+    actionCopyCidAll = QAction("hjpCopyCidAlltoInputJson", browser)
+    actionCopyCidAll.triggered.connect(lambda _, b=browser: multicopyFunction(b))
     menu.addAction(actionCopyCidAll)
+    actionGroupCopy = QAction("hjpAsGroupCopytoInputJson", browser)
+    actionGroupCopy.triggered.connect(lambda _, b=browser: multicopyFunction(b, groupCopy=True))
+    menu.addAction(actionGroupCopy)
+
+
 gui_hooks.browser_menus_did_init.append(setUpMenuShortcut)
 gui_hooks.browser_will_show_context_menu.append(AddToTableContextMenu)
-#linkActToMainMenu()
-
+# linkActToMainMenu()
