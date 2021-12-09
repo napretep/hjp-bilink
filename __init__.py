2bf9d00ab6b4d84868564a69547c55d23e8dce35
564298339@qq.com <564298339@qq.com>
2021-12-09 11:17:38 +0800

    hjp-bilink2.4

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 
 """
 from .lib.common_tools import connectors
-connectors.funcs.G.src.ADDON_VERSION="2.4.0beta2"
+connectors.funcs.G.src.ADDON_VERSION="2.4.1"
 connectors.funcs.G.ISDEBUG=False
 
 connectors.run()
