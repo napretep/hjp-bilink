4ff983dcf577879d195f87fe474d09d58ab6e545
十五 <napretep1992@gmail.com>
2023-02-27 06:25:41 +0800

    测试vscode提交

---
+++ lib/common_tools/all_models/imports.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+__project_ = 'hjp-bilink'
+__file_name__ = 'imports.py'
+__author__ = '十五'
+__email__ = '564298339@qq.com'
+__time__ = '2023/2/27 6:14'
+"""
---
+++ lib/common_tools/all_models/view_self_models.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+__project_ = 'hjp-bilink'
+__file_name__ = 'view_self_models.py'
+__author__ = '十五'
+__email__ = '564298339@qq.com'
+__time__ = '2023/2/27 5:56'
+"""
