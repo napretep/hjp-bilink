4b71fcb958d2397c777c48ab8d30f9c18ae07ba9
十五 <napretep1992@gmail.com>
2023-03-10 00:53:17 +0800

    hjp-LinkMaster2.6.0 beta8.2 转移到linux上继续编辑

---
+++ lib/common_tools/all_models/view_create_dialog_model.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+__project_ = 'hjp-bilink'
+__file_name__ = 'view_create_dialog_model.py'
+__author__ = '十五'
+__email__ = '564298339@qq.com'
+__time__ = '2023/3/10 0:53'
+"""
